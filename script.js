0539a00f73cabee971a5d0668f847d8329221c8d
Sara Pellnor <114143422+SaraPellnor@users.noreply.github.com>
2023-05-12 20:57:33 +0200

    json fil och script + lite småpill

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-const star = document.querySelectorAll(".fa-star")
-star.forEach(element => {
-    element.addEventListener("click", () => {
-        element.style.color = "yellow"
-        
-    } )
-});
-const readMoreBtn = document.querySelector('.read-more-btn');
-const hiddenText = document.querySelector('.hidden-text');
-const parent = readMoreBtn.parent
-console.log(parent);
-readMoreBtn.addEventListener('click', () => {
-        hiddenText.style.display = 'block';
-        readMoreBtn.style.display = 'none';
-      });
+// Hämtar data från posts.json
+async function fetchPosts() {
+    try {
+      const response = await fetch("./posts.json");
+  
+      if (!response.ok) {
+        throw new Error("Något gick fel vid hämtning av data.");
+      }
+  
+      const data = await response.json();
+      appendPosts(data)
 
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  fetchPosts() 
+
+// visar objekt från post.json till posts.html   
+function appendPosts(data) {
+    
+    data.forEach(element => {
+    const posts = document.querySelector(".posts")
+    const section = document.createElement("section")
+    const h2 = document.createElement("h2")
+    const a = document.createElement("a")
+    const img = document.createElement("img")
+    const p = document.createElement("p")
+
+    section.setAttribute("class", element.name)
+    a.setAttribute("href", element.link)
+    img.setAttribute("src", element.image)
+
+
+    h2.innerText = element.name
+    p.innerText = element.description
+
+    a.append(img)
+    section.append(h2, a, p)
+    posts.append(section)
+
+    });
+  }
